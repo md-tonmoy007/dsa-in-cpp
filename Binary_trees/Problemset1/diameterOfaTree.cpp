d23e480fe22d2847c7c09f6f4c25df0c43f3a04a
IamMrRobot <iammrrobot@MrRobot.MrRobot>
2024-05-10 16:01:16 +0600

    added

--- Binary_trees/Problemset1/diameterOfaTree.cpp
+++ Binary_trees/Problemset1/diameterOfaTree.cpp
@@ -54,7 +54,11 @@ int main() {
     // int Diameter = getDiameter(root);
     pair<int, int> Diameter = getDiameterOptimised(root);
 
+    int d = getDiameter(root);
+
     cout << endl << "Diameter of the tree is : " << Diameter.first << endl;
+
+    cout << endl << "not optimised: Diameter=> "<< d << endl;
 
     return 0;
 }
@@ -64,4 +68,28 @@ int main() {
         3 4 -1 -1 6 -1 -1
         5 4 2 -1 6 -1 -1 7 -1 8 -1 9 -1 -1 10 -1 -1
         8 2 4 -1 -1 6 2 -1 -1 5 -1 -1 5 -1 9 -1 6 4 2 -1 -1 3 -1 -1 7 -1 -1
+*/
+
+
+/*
+
+
+1
+2
+4
+8
+-1
+-1
+5
+-1
+-1
+3
+6
+-1
+-1
+7
+-1
+-1
+
+
 */
